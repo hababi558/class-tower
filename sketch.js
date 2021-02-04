72f5d73aa4ea6949cd79d553a390a140a82a49ac
hababi558 <74720738+hababi558@users.noreply.github.com>
2021-02-04 19:05:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world
+
+function setup(){
+    createCanvas(1200,600)
+    engine = Engine.create();
+    world = engine.world;
+
+    
+}
+
+function draw(){
+background(0)
+}
